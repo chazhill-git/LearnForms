d93d896e90dd4b4c82a215a8af09494e1538c8fe
chazhill-git <chazhill345@gmail.com>
2020-01-10 16:49:28 -0800

    First commit

---
+++ BAL/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BAL.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
